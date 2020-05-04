1f04eafaed48abf5c770b05ce2239d0c002387e4
DreghiciuAnca <anca_dreghiciu_1997@yahoo.com>
2020-05-04 12:49:08 +0300

    Bonus 2-B

--- Proiect/Neural Network.py
+++ Proiect/Neural Network.py
@@ -47,19 +47,29 @@ class OurNeuralNetwork:
     self.w7 = np.random.normal()
     self.w8 = np.random.normal()
     self.w9 = np.random.normal()
+    self.w10 = np.random.normal()
+    self.w11 = np.random.normal()
+    self.w12 = np.random.normal()
+    self.w13 = np.random.normal()
+    self.w14 = np.random.normal()
+
 
     # Biases
     self.b1 = np.random.normal()
     self.b2 = np.random.normal()
-    self.b3 = np.random.normal()
-    self.b4 = np.random.normal()
+    self.b3 = np.random.normal()  # output
+    self.b4 = np.random.normal()  # h3
+    self.b5 = np.random.normal()  # h4
+    self.b6 = np.random.normal()  # h5
 
   def feedforward(self, x):
     # x is a numpy array with 2 elements.
-    h1 = sigmoid(self.w1 * x[0] + self.w2 * x[1] + self.b1)
-    h2 = sigmoid(self.w3 * x[0] + self.w4 * x[1] + self.b2)
-    h3 = sigmoid(self.w7 * x[0] + self.w8 * x[1] + self.b4)
-    o1 = sigmoid(self.w5 * h1 + self.w6 * h2 + self.b3)
+    h1 = relu(self.w1 * x[0] + self.w2 * x[1] + self.b1)
+    h2 = relu(self.w3 * x[0] + self.w4 * x[1] + self.b2)
+    h3 = relu(self.w7 * x[0] + self.w8 * x[1] + self.b4)
+    h4 = relu(self.w5 * h1 + self.w6 * h2 + self.w9 * h3 + self.b5)
+    h5 = relu(self.w10 * h1 + self.w11 * h2 + self.w12 * h3 + self.b6)
+    o1 = relu(self.w5 * h1 + self.w6 * h2 + self.b3)
     return o1
 
   def train(self, data, all_y_trues):
@@ -83,7 +93,13 @@ class OurNeuralNetwork:
         sum_h3 = self.w7 * x[0] + self.w8 * x[1] +self.b4
         h3 = relu(sum_h3)
 
-        sum_o1 = self.w5 * h1 + self.w6 * h2 + self.w9 * h3 + self.b3
+        sum_h4 = self.w5 * h1 + self.w6 * h2 + self.w9 * h3 + self.b5
+        h4 = relu(sum_h4)
+
+        sum_h5 = self.w10 * h1 + self.w11 * h2 + self.w12 * h3 + self.b6
+        h5 = relu(sum_h5)
+
+        sum_o1 = self.w13 * h4 + self.w14 * h5 + self.b3
         o1 = relu(sum_o1)
         y_pred = o1
 
@@ -92,14 +108,16 @@ class OurNeuralNetwork:
         d_L_d_ypred = -2 * (y_true - y_pred)
 
         # Neuron o1
-        d_ypred_d_w5 = h1 * deriv_relu(sum_o1)
-        d_ypred_d_w6 = h2 * deriv_relu(sum_o1)
-        d_ypred_d_w9 = h3 * deriv_relu(sum_o1)
+        d_ypred_d_w13 = h4 * deriv_relu(sum_o1)
+        d_ypred_d_w14 = h5 * deriv_relu(sum_o1)
         d_ypred_d_b3 = deriv_relu(sum_o1)
 
-        d_ypred_d_h1 = self.w5 * deriv_relu(sum_o1)
-        d_ypred_d_h2 = self.w6 * deriv_relu(sum_o1)
-        d_ypred_d_h3 = self.w9 * deriv_relu(sum_o1)
+        d_ypred_d_h1 = self.w5 * deriv_relu(sum_h4) * self.w10 * deriv_relu(sum_h5)
+        d_ypred_d_h2 = self.w6 * deriv_relu(sum_h4) * self.w11 * deriv_relu(sum_h5)
+        d_ypred_d_h3 = self.w9 * deriv_relu(sum_h4) * self.w12 * deriv_relu(sum_h5)
+
+        d_ypred_d_h4 = self.w13 * deriv_relu(sum_o1)
+        d_ypred_d_h5 = self.w14 * deriv_relu(sum_o1)
 
         # Neuron h1
         d_h1_d_w1 = x[0] * deriv_relu(sum_h1)
@@ -114,7 +132,19 @@ class OurNeuralNetwork:
         # Neuron h3
         d_h3_d_w7 = x[0] * deriv_relu(sum_h3)
         d_h3_d_w8 = x[1] * deriv_relu(sum_h3)
-        d_h3_d_b3 = deriv_relu(sum_h3)
+        d_h3_d_b4 = deriv_relu(sum_h3)
+
+        # Neuron h4
+        d_h4_d_w5 = deriv_relu(sum_h1) * deriv_relu(sum_h4)
+        d_h4_d_w6 = deriv_relu(sum_h2) * deriv_relu(sum_h4)
+        d_h4_d_w9 = deriv_relu(sum_h3) * deriv_relu(sum_h4)
+        d_h4_d_b5 = deriv_relu(sum_h4)
+
+        # Neuron h5
+        d_h5_d_w10 = deriv_relu(sum_h1) * deriv_relu(sum_h5)
+        d_h5_d_w11 = deriv_relu(sum_h2) * deriv_relu(sum_h5)
+        d_h5_d_w12 = deriv_relu(sum_h3) * deriv_relu(sum_h5)
+        d_h5_d_b6 = deriv_relu(sum_h5)
 
         # --- Update weights and biases
         # Neuron h1
@@ -130,13 +160,24 @@ class OurNeuralNetwork:
         # Neuron h3
         self.w7 -= learn_rate * d_L_d_ypred * d_ypred_d_h3 * d_h3_d_w7
         self.w8 -= learn_rate * d_L_d_ypred * d_ypred_d_h3 * d_h3_d_w8
-        self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_h3 * d_h3_d_b3
+        self.b4 -= learn_rate * d_L_d_ypred * d_ypred_d_h3 * d_h3_d_b4
+
+        # Neuron h4
+        self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_h4 * d_h4_d_w5
+        self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_h4 * d_h4_d_w6
+        self.w9 -= learn_rate * d_L_d_ypred * d_ypred_d_h4 * d_h4_d_w9
+        self.b5 -= learn_rate * d_L_d_ypred * d_ypred_d_h4 * d_h4_d_b5
+
+        # Neuron h5
+        self.w10 -= learn_rate * d_L_d_ypred * d_ypred_d_h5 * d_h5_d_w10
+        self.w11 -= learn_rate * d_L_d_ypred * d_ypred_d_h5 * d_h5_d_w11
+        self.w12 -= learn_rate * d_L_d_ypred * d_ypred_d_h5 * d_h5_d_w12
+        self.b5 -= learn_rate * d_L_d_ypred * d_ypred_d_h5 * d_h5_d_b6
 
 
         # Neuron o1
-        self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
-        self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
-        self.w9 -= learn_rate * d_L_d_ypred * d_ypred_d_w9
+        self.w13 -= learn_rate * d_L_d_ypred * d_ypred_d_w13
+        self.w14 -= learn_rate * d_L_d_ypred * d_ypred_d_w14
         self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
 
       # --- Calculate total loss at the end of each epoch
@@ -154,8 +195,8 @@ class OurNeuralNetwork:
 #])
 
 
-woman_height = np.round(np.random.normal(2, 0.50, 2), 0)
-woman_weight = np.round(np.random.normal(2, 0.50, 2), 0)
+woman_height = np.round(np.random.normal(1, 10, 2), 0)
+woman_weight = np.round(np.random.normal(1, 10, 2), 0)
 man_height = np.round(np.random.normal(5, 15, 2), 0)
 man_weight = np.round(np.random.normal(5, 15, 2), 0)
 np_woman = np.column_stack((woman_height, woman_weight))
